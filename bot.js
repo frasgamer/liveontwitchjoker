ffe3cec83fc5c8018bb48f61fb3cce351f5aa484
frasgamer <44925264+frasgamer@users.noreply.github.com>
2019-01-04 19:52:03 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('ready', function(){	
+    var ms = 40000 ;	
+    var setGame = ['by Fras#9999','عندما تحب عدوك يحس بتفاهته','اختار صديقك لاتختار جحيمك'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
+}, ms);	
+	
+});
+
+client.login(process.env.BOT_TOKEN);
