23f29158d4dc2291466c9ec7196075169ac1727a
nasrfhqgjh <44925264+frasgamer@users.noreply.github.com>
2019-01-22 21:59:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,47 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('ready', function(){	
-    var ms = 40000 ;	
-    var setGame = ['عندما تحب عدوك يحس بتفاهته','اختار صديقك لاتختار جحيمك'];	
-    var i = -1;	
-    var j = 0;	
-    setInterval(function (){	
-        if( i == -1 ){	
-j = 1;	
-       }	
-        if( i == (setGame.length)-1 ){	
-            j = -1;	
-      }	
-       i = i+j;	
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
-}, ms);	
-	
+
+
+
+
+
+
+
+
+
+const adminprefix = "Z";
+const devs = ['307666420743798784','313377631783485440'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'play')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'name')) {//fras
+client.user.setUsername(argresult).then//fras
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)//fras
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");//fras
+} else//fras
+  if (message.content.startsWith(adminprefix + 'avatar')) { //fras
+client.user.setAvatar(argresult);//fras
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);//fras
+      } else     //fras
+if (message.content.startsWith(adminprefix + 'streaming')) {//fras
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");//Fras
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)//fras
+}
 });
+
+
+
 
 client.login(process.env.BOT_TOKEN);
